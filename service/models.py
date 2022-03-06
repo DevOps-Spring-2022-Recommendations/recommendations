ce1c2714d93d2267df74f9787409727f8e7f40eb
mkcz <99369789+mkcz@users.noreply.github.com>
2022-03-06 16:51:47 -0500

    Update models.py

--- service/models.py
+++ service/models.py
@@ -104,5 +104,9 @@ class Recommendation(db.Model):
         db.init_app(app)
         app.app_context().push()
         db.create_all()  # make our sqlalchemy tables
-
+        
+    def all(cls) -> list:
+        """ Returns all of the Products in the database """
+        logger.info("Processing all Products")
+        return cls.query.all()
 
